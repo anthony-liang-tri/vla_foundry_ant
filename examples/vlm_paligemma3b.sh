62d98767ea3546a3be8278319436c5432dc69cb8
Jean Mercat <jean.mercat@tri.global>
2025-07-25 19:50:56 -0700

    update examples and yaml files

--- examples/vlm_paligemma3b.sh
+++ examples/vlm_paligemma3b.sh
@@ -1,8 +1,6 @@
 .venv/bin/torchrun --nproc_per_node=8 --nnodes=1 lbm2/main.py \
---model.type vlm \
---model.transformer !include lbm2/config_presets/models/vlm_3b.yaml \
---model.vit.type vit \
---model.vit !include lbm2/config_presets/models/vit_paligemma.yaml \
+--model.transformer="include lbm2/config_presets/models/vlm_3b.yaml" \
+--model.vit="include lbm2/config_presets/models/vit_paligemma.yaml" \
 --distributed.fsdp True \
 --distributed.fsdp_use_orig_params True \
 --distributed.fsdp_limit_all_gathers True \
