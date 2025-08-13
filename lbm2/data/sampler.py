57468ce081d321bcac8131761347408de80362bc
Jean Mercat <jean.mercat@tri.global>
2025-08-13 09:10:38 -0700

    allow sample chunk to sample the whole sequence if it is smaller than the set seq_len

--- lbm2/data/sampler.py
+++ lbm2/data/sampler.py
@@ -7,7 +7,9 @@ def sample_chunk(input_ids, attention_mask, seq_len):
     elif input_ids.shape[1] > seq_len + 1:
         start_idx = torch.randint(0, input_ids.shape[1] - seq_len, (1,)).item()
     else:
-        raise Exception(f"Invalid sequence length: Sequence length {seq_len} > {input_ids.shape[1]} Chunk size")
+        seq_len = input_ids.shape[1]
+        out_attention_mask = attention_mask[:, : seq_len - 1] if attention_mask is not None else None
+        return input_ids[:, : seq_len - 1], out_attention_mask, input_ids[:, 1:seq_len]
 
     inputs = input_ids[:, start_idx : start_idx + seq_len]
     mask = attention_mask[:, start_idx : start_idx + seq_len] if attention_mask is not None else None
