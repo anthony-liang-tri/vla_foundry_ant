1771a74a886ab1d6c19e59095a8ed6203e37d131
sedrick-keh-tri <sedrick.keh@tri.global>
2025-09-08 21:53:51 +0000

    add eval runners

---
+++ lbm2/eval/README.md
@@ -0,0 +1,3 @@
+# Eval
+
+This folder is almost entirely self-contained. Might considering moving this to a separate repo at some point.
---
+++ lbm2/eval/runners/__init__.py
@@ -0,0 +1,11 @@
+from lbm2.eval.runners.robocasa import RoboCasaEvalRunner
+from lbm2.eval.runners.libero import LiberoRunner
+
+
+def get_eval_runner(eval_params):
+    if eval_params.env == "robocasa":
+        return RoboCasaEvalRunner(eval_params)
+    elif eval_params.env == "libero":
+        return LiberoRunner(eval_params)
+    else:
+        raise ValueError(f"Invalid environment name: {eval_params.env}")
