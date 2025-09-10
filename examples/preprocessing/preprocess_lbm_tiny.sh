309ea54b9e075999a279cb42f52b02f58a865359
paarth-shah <paarth.shah@tri.global>
2025-09-10 12:35:46 -0400

    Adding actions to output of streaming shard writer

--- examples/preprocessing/preprocess_lbm_tiny.sh
+++ examples/preprocessing/preprocess_lbm_tiny.sh
@@ -1,7 +1,7 @@
     # --source_episodes "include examples/preprocessing/test_list.yaml" \
 uv run --group preprocessing python lbm2/data/preprocessing/preprocess_lbm_data.py \
     --source_episodes "['s3://robotics-manip-lbm/efs/data/tasks/PickAndPlaceBox/cabot/sim/bc/teleop/2025-02-11T17-04-00-05-00/']" \
-    --output_dir s3://tri-ml-datasets/preprocess_test_tiny/lbm/PickAndPlaceBox/cabot/sim/ \
+    --output_dir s3://tri-ml-datasets/preprocess_lbm_test/lbm/PickAndPlaceBox/cabot/sim/ \
     --language_annotations_path lbm2/data/preprocessing/lbm_language_annotations.yaml \
     --discard_keys "include lbm2/config_presets/data/lbm_data_discard_key.yaml" \
     --camera_names "include lbm2/config_presets/data/lbm_data_camera_names.yaml" \
