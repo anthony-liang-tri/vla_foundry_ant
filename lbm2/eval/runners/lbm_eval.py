19e5dca347b6d5cf10082f7b6fcaeea24d80375f
katherine.liu <katherine.liu@tri.global>
2025-09-23 11:33:23 -0700

    Evaluate must be imported first; this is a quirk

--- lbm2/eval/runners/lbm_eval.py
+++ lbm2/eval/runners/lbm_eval.py
@@ -11,8 +11,13 @@ uv run lbm2/eval/run_eval.py --env lbm_eval --task pick_and_place_box
 import copy
 from dataclasses import dataclass
 from pathlib import Path
-
 import numpy as np
+
+# evaluate must be imported before the other anzu libraries
+from lbm_eval.evaluate import _LastStepRecorder
+from pydrake.common.yaml import yaml_load
+from robot_gym.multiarm_spaces import PosesAndGrippers
+
 from anzu.common.anzu_model_directives import MakeDefaultAnzuPackageMap
 from anzu.intuitive.typing_ import from_dict
 from anzu.intuitive.visuomotor.bases import (
@@ -22,11 +27,6 @@ from anzu.intuitive.visuomotor.demonstration_seed import get_demonstration_seed
 from anzu.intuitive.visuomotor.multiarm_simulations import (
     HardwareStationScenarioSimulationEnvConfig,
 )
-
-# evaluate must be imported before the other anzu libraries
-from lbm_eval.evaluate import _LastStepRecorder
-from pydrake.common.yaml import yaml_load
-from robot_gym.multiarm_spaces import PosesAndGrippers
 
 # lbm2 imports
 from lbm2.eval.runners.base_eval_runner import BaseEvalRunner
