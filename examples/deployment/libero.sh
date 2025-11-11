7e1f8c3cebfc0888e0e7b46ea442d15bb158d8e0
sedrick-keh-tri <133716510+sedrick-keh-tri@users.noreply.github.com>
2025-11-10 19:28:49 -0800

    lbm2->vla_foundry in eval runners (#346)

--- examples/deployment/libero.sh
+++ examples/deployment/libero.sh
@@ -1,4 +1,4 @@
-python lbm2/eval/run_eval.py \
+python vla_foundry/eval/run_eval.py \
 --env libero_goal \
 --task 8 \
 --model_path s3://tri-ml-datasets/lbm2/model_checkpoints/libero/PutBowlOnPlate/2025_10_08-18_44_40-model_diffusion_policy-lr_0.0005-bsz_2048/ \
