37fc8ca054f1a9e7b90a48f39cbf16defdf7692f
jmercat <jean.mercat@tri.global>
2025-12-02 18:31:59 -0800

    fast image decode, use prefetch and pin memory (#435)
    * fast image decode, use prefetch and pin memory
    
    * bump transformers version makes it faster
    
    * make opencv a training dependency

--- vla_foundry/distributed.py
+++ vla_foundry/distributed.py
@@ -201,10 +201,8 @@ def wrap_fsdp_ddp(model, device, cfg):
 
         ddp_args = {
             "find_unused_parameters": True,  # Handle unused parameters in DDP
+            "static_graph": True,
         }
-        if cfg.distributed.ddp_static_graph:
-            # this doesn't exist in older PyTorch, arg only added if enabled
-            ddp_args["static_graph"] = True
         model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[device], **ddp_args)
 
     return model
