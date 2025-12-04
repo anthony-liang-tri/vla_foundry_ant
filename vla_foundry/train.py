4741f28e38e2a17482ddd666f99301b4ecda6d37
paarth-shah <paarth.shah@tri.global>
2025-12-04 09:58:54 -0500

    Reverting FSDP fix so I can move to another PR

--- vla_foundry/train.py
+++ vla_foundry/train.py
@@ -1,3 +1,4 @@
+
 import itertools
 import logging
 import time
@@ -8,7 +9,6 @@ import torch.distributed as dist
 import torch.nn as nn
 import torch.optim as optim
 from torch.distributed.distributed_c10d import ReduceOp
-from torch.distributed.fsdp import FSDPModule
 from tqdm import tqdm
 
 from vla_foundry.distributed import is_master
@@ -112,8 +112,7 @@ def train_one_checkpoint(
         total_lm_loss = 0
         for ii in range(cfg.hparams.accum_freq):
             # Don't sync gradients until the final microbatch for FSDP.
-            # Check if model is actually wrapped with FSDP (not just if FSDP is enabled in config)
-            if isinstance(model, FSDPModule):
+            if cfg.distributed.fsdp:
                 is_final_accum = ii == cfg.hparams.accum_freq - 1
                 model.set_requires_gradient_sync(is_final_accum)
                 model.set_requires_all_reduce(is_final_accum)
