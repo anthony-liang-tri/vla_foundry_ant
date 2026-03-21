2fd2c29f8e6ea26d74c21cfd3a6943300a6af640
Kushal Arora <kushal.arora@tri.global>
2026-03-21 15:02:40 -0700

    Revert "Configs for smolvlm and added multi-task nominal and add support in generate_ablation_configs.py for multitask ablations."
    This reverts commit e95b993162054dd7ecd1685316969eb6d7c9b508.

--- examples/training/vla_diffusion_redbellpepper_smolvlm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Train VLADiffusion model with SmolVLM encoder on robotics data
-
-.venv/bin/torchrun --nproc_per_node=1 --nnodes=1 vla_foundry/main.py \
---config_path vla_foundry/config_presets/training_jobs/vla_diffusion_bellpepper_smolvlm_256m.yaml \
---remote_sync s3://tri-ml-datasets-uw2/lbm2_vla/model_checkpoints/vla_diffusion \
---num_checkpoints 5 \
---total_train_samples 1000 \
-"$@"
